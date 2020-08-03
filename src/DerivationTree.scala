408d424fe998e31c319c8ee70edb13e9d23cfd20
Liv <kondrup.liv@gmail.com>
2020-08-03 13:31:06 +0200

    DerivationTree: The trees can be build

--- src/DerivationTree.scala
+++ src/DerivationTree.scala
@@ -1,3 +1,9 @@
-class DerivationTree {
+type DerivationNode = (String, DerivationNode)
+class DerivationTree(initialVariable: String) {
+  private val initialNode = (initialVariable, initialNode)
+  private var nodes: Set[DerivationNode] = Set(initialNode)
 
+  def addChild(rule: Rule, parentRule: Rule): Unit = {
+    nodes = nodes + (rule, parentRule)
+  }
 }
