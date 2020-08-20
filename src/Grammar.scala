8f8582c56b4e592c12ba8d802efe5d8394270501
Liv <kondrup.liv@gmail.com>
2020-08-20 11:36:34 +0200

    Using JUnit instead of scalatest

--- src/Grammar.scala
+++ src/Grammar.scala
@@ -1,2 +1,14 @@
 class Grammar(rules: List[Rule], start:Rule) {
+
+  def ConvertToCNF():Unit = {
+    removeLambda()
+    removeChains()
+    fixRighSides()
+  }
+
+  private def removeLambda():Unit = ???
+
+  private def removeChains():Unit = ???
+
+  private def fixRighSides():Unit = ???
 }
