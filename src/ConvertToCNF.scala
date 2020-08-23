b0ae539bc7fe7b1d5991419cb0aab35ef23ed787
Liv <kondrup.liv@gmail.com>
2020-08-23 13:45:55 +0200

    Cleaned up for different warnings

--- src/ConvertToCNF.scala
+++ src/ConvertToCNF.scala
@@ -1,11 +1,12 @@
-import scala.{::, List}
+
 
 object ConvertToCNF {
-  def getGrammarOnCNF(grammar: Grammar):Unit = {
+  def getGrammarOnCNF(grammar: Grammar):Grammar = {
     var convertedGrammar = grammar
     convertedGrammar = eliminateLambda(convertedGrammar)
     convertedGrammar = eliminateChains(convertedGrammar)
-    convertedGrammar = fixRighSides(convertedGrammar)
+    convertedGrammar = fixRightSides(convertedGrammar)
+    convertedGrammar
   }
 
   def eliminateLambda(grammar:Grammar):Grammar = {
@@ -19,17 +20,17 @@ object ConvertToCNF {
         rulesInNewGrammar += new Rule(rule.getLeft(), newRight)  // Adds the rule WITHOUT the variables that are nullable (and also that is not lambda)
       }
     }
-    for (rule <- rulesInNewGrammar){    // This loop removes rules that does not have anything on the rightside
-                                        // The rules happens when the subset in loop above is all of the variables on the rightside
+    for (rule <- rulesInNewGrammar){    // This loop removes rules that does not have anything on the right-side
+                                        // The rules happens when the subset in loop above is all of the variables on the right-side
       if (rule.getRight().isEmpty){
         rulesInNewGrammar = rulesInNewGrammar - rule
       }
     }
-    return new Grammar(rulesInNewGrammar, grammar.getStartVariable())
+    new Grammar(rulesInNewGrammar, grammar.getStartVariable())
   }
 
   def findNullables(grammar: Grammar): Set[String] = {
-    return (findNullableVariables(grammar, findLambdaVariables(grammar)))
+    findNullableVariables(grammar, findLambdaVariables(grammar))
   }
 
   def findLambdaVariables(grammar: Grammar):Set[String] = {
@@ -46,7 +47,7 @@ object ConvertToCNF {
         nullable += rule.getLeft()
       }
     }
-    return nullable
+    nullable
   }
 
   def findNullableVariables(grammar: Grammar, lambdaVariables:Set[String]):Set[String] = {
@@ -68,14 +69,14 @@ object ConvertToCNF {
       }
       nullableChanged = !nullable.equals(nullableBefore)   // If the list of nullables have changed, the while-loop should keep going
     }
-    return nullable
+    nullable
   }
 
   def eliminateChains(grammar: Grammar):Grammar = {
     var newRules = Set[Rule]()
     for (rule <- grammar.getRules()){     // First run of finding new rules
       if(rule.isChainRule()){     // If a rule is a chain, something special needs to happen, otherwise it is just added to the new grammar
-        val rightVar = rule.getRight()(0)   // If a rule is a chain rule, there is only one element in the right-side
+        val rightVar = rule.getRight().head   // If a rule is a chain rule, there is only one element in the right-side
         val rulesFromRightVar = grammar.getRules().filter(r => r.getLeft().equals(rightVar))    // Set of all right-sides of rules with the right-side of the chain on the left
         for (r <- rulesFromRightVar){
           newRules += new Rule(rule.getLeft(), r.getRight())    // Adding all rules that has the right of the chain-rule on the left
@@ -92,7 +93,7 @@ object ConvertToCNF {
       for(rule <- newRules){    // Go through all of the rules
         if(rule.isChainRule()){   // If a rule is a chain rule
           newRules -= rule      // It is removed form the list
-          val rightVar = rule.getRight()(0)
+          val rightVar = rule.getRight().head
           val rulesFromRightVar = newRules.filter(r => r.getLeft().equals(rightVar))    // The right-sides of rules are found in the updated set, not the original
           for (r <- rulesFromRightVar){
             newRules += new Rule(rule.getLeft(), r.getRight())    // The rules to replace the chain rule is added
@@ -108,9 +109,9 @@ object ConvertToCNF {
       }
       chainRulesInNewRules = stillChainRules
     }
-    return new Grammar(newRules, grammar.getStartVariable())
+    new Grammar(newRules, grammar.getStartVariable())
   }
 
-  def fixRighSides(grammar: Grammar):Grammar = ???
+  def fixRightSides(grammar: Grammar):Grammar = ???
 
 }
